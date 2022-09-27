775aea3cc4a8651bc719d3adf5626736069cdd86
Nisha <102697756+Nisha9724@users.noreply.github.com>
2022-09-27 20:39:59 +0530

    added color

--- apps.js
+++ apps.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
+const colors = ["green", "FFA500", "rgb(70, 192, 196)", "#f15025", "808000", "maroon", "#DB77A9"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
@@ -6,7 +6,7 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function(){
 
 const randomNumber = getRandomNumber();
-console.log(randomNumber);
+
 
 document.body.style.backgroundColor = colors[randomNumber];
 color.textContent = colors[randomNumber];
